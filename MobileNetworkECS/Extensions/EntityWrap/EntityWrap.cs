68ab39560c21b4301a917b160191fba3b68cd6df
Darkhhh <82178107+Darkhhh@users.noreply.github.com>
2023-10-10 22:55:36 +0300

    Modified EntityWrap.cs for more usability

--- MobileNetworkECS/Extensions/EntityWrap/EntityWrap.cs
+++ MobileNetworkECS/Extensions/EntityWrap/EntityWrap.cs
@@ -1,3 +1,4 @@
+using MobileNetworkECS.Core.Entities;
 using MobileNetworkECS.Core.Worlds;
 
 namespace MobileNetworkECS.Extensions.EntityWrap;
@@ -7,29 +8,41 @@ public abstract class EntityWrapBase
     public abstract object GetRawValue();
     public abstract int GetEntityId();
     public abstract IEcsWorld GetWorld();
+    public abstract bool TryGetAs<T>(out T result);
+    public abstract Entity GetEntity();
 }
 
 public class EntityWrap<T> : EntityWrapBase where T : class
 {
-    public T Value { get; init; }
+    public T Value { get; }
+    private readonly Entity _entity;
 
     private readonly IEcsWorld _world;
-    private readonly int _entity;
+    private readonly int _entityId;
     
     public EntityWrap(T item, IEcsWorld world)
     {
         Value = item;
         _world = world;
-
+        
         var wrapPool = _world.GetPool<EntityWrapComponent>();
-        _entity = _world.CreateEntity();
-        ref var data = ref wrapPool.Add(_entity);
+        _entityId = _world.NewEntityId();
+        _entity = _world.GetEntityById(_entityId);
+        ref var data = ref wrapPool.Add(_entityId);
         data.Reference = this;
     }
 
     public override object GetRawValue() => Value;
 
-    public override int GetEntityId() => _entity;
+    public override int GetEntityId() => _entityId;
 
     public override IEcsWorld GetWorld() => _world;
+    public override bool TryGetAs<T1>(out T1 result)
+    {
+        result = default!;
+        if (Value is not T1 r) return false;
+        result = r;
+        return true;
+    }
+    public override Entity GetEntity() => _entity;
 }
